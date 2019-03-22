2b6e13a4b53927f3f7c9cb8ed93a49bd3ae2c5ff
chenzhong.cz <chenzhong.cz@alibaba-inc.com>
2019-03-22 22:23:03 +0800

    support editor psi tree

--- src/scala/edu/jetbrains/plugin/lt/MyLiveTemplatesConfigurable.java
+++ src/scala/edu/jetbrains/plugin/lt/MyLiveTemplatesConfigurable.java
@@ -1,12 +1,16 @@
 package scala.edu.jetbrains.plugin.lt;
 
 import com.intellij.codeInsight.template.impl.LiveTemplatesConfigurable;
+import com.intellij.codeInsight.template.impl.TemplateListPanel;
 
 import javax.swing.JComponent;
 
 public class MyLiveTemplatesConfigurable extends LiveTemplatesConfigurable {
+    private TemplateListPanel myPanel;
+
     @Override
     public JComponent createComponent() {
-        return super.createComponent();
+        myPanel = (TemplateListPanel) super.createComponent();
+        return myPanel;
     }
 }
