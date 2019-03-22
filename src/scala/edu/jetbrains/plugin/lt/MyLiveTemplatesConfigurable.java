0a5b784091c98cbfb2c0bf8455671e095f4200cc
chenzhong.cz <chenzhong.cz@alibaba-inc.com>
2019-03-22 22:52:34 +0800

    support editor psi tree

--- src/scala/edu/jetbrains/plugin/lt/MyLiveTemplatesConfigurable.java
+++ src/scala/edu/jetbrains/plugin/lt/MyLiveTemplatesConfigurable.java
@@ -3,10 +3,14 @@ package scala.edu.jetbrains.plugin.lt;
 import com.intellij.codeInsight.template.impl.LiveTemplatesConfigurable;
 import com.intellij.codeInsight.template.impl.TemplateListPanel;
 import com.intellij.ui.CheckboxTree;
+import com.intellij.util.ui.tree.TreeModelAdapter;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
 
 import javax.swing.JComponent;
+import javax.swing.event.TreeModelEvent;
 
 public class MyLiveTemplatesConfigurable extends LiveTemplatesConfigurable {
     private TemplateListPanel myPanel;
@@ -18,6 +22,12 @@ public class MyLiveTemplatesConfigurable extends LiveTemplatesConfigurable {
             Field myTreeField = TemplateListPanel.class.getDeclaredField("myTree");
             myTreeField.setAccessible(true);
             CheckboxTree myTreeValue = (CheckboxTree) myTreeField.get(myPanel);
+            myTreeValue.getModel().addTreeModelListener(new TreeModelAdapter() {
+                @Override
+                protected void process(@NotNull TreeModelEvent event, @NotNull EventType type) {
+                }
+            });
+
         } catch (Exception exc) {
             exc.printStackTrace();
             throw new IllegalStateException(exc.getMessage());
