dc6e67b914d33764ca876ca83a1254d41cd0ce9b
chenzhong.cz <chenzhong.cz@alibaba-inc.com>
2019-03-22 22:31:37 +0800

    support editor psi tree

--- src/scala/edu/jetbrains/plugin/lt/MyLiveTemplatesConfigurable.java
+++ src/scala/edu/jetbrains/plugin/lt/MyLiveTemplatesConfigurable.java
@@ -2,6 +2,9 @@ package scala.edu.jetbrains.plugin.lt;
 
 import com.intellij.codeInsight.template.impl.LiveTemplatesConfigurable;
 import com.intellij.codeInsight.template.impl.TemplateListPanel;
+import com.intellij.ui.CheckboxTree;
+
+import java.lang.reflect.Field;
 
 import javax.swing.JComponent;
 
@@ -11,6 +14,14 @@ public class MyLiveTemplatesConfigurable extends LiveTemplatesConfigurable {
     @Override
     public JComponent createComponent() {
         myPanel = (TemplateListPanel) super.createComponent();
+        try {
+            Field myTreeField = TemplateListPanel.class.getDeclaredField("myTree");
+            myTreeField.setAccessible(true);
+            CheckboxTree myTreeValue = (CheckboxTree) myTreeField.get(myPanel);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            throw new IllegalStateException(exc.getMessage());
+        }
         return myPanel;
     }
 }
